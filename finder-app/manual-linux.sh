3672cea79d6775a857b730590c2c82ed9936be19
vtewari <vtewari@Ubuntu.myguest.virtualbox.org>
2024-12-23 02:11:56 -0500

    assignmnet 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Script outline to install and build kernel.
 # Author: Siddhant Jajoo.
-
 set -e
 set -u
 
@@ -12,6 +11,9 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+
+#run from finder-app directory
+current_directory=$(pwd)
 
 if [ $# -lt 1 ]
 then
@@ -29,14 +31,32 @@ if [ ! -d "${OUTDIR}/linux-stable" ]; then
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
+
+
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # Add your kernel build steps here
+    # Clean the kernel build tree 
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+  
+    # Configure the kernel 
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+#: <<'COMMENT'    
+    # Build the kernel image 
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    
+    # Build the modules 
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    
+    # Install the modules 
+    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules_install 
+    
+    # Build the device tree blobs 
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
-
 echo "Adding the Image in outdir"
 
 echo "Creating the staging directory for the root filesystem"
@@ -47,7 +67,8 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs/{bin,dev,etc,home,lib,lib64,proc,sbin,sys,tmp,usr/{bin,lib,sbin},var/log}
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +76,49 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# Make and install busybox
+make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Add library dependencies to rootfs
+SYSROOT=$(${CROSS_COMPILE}gcc --print-sysroot)
+cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp -a ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp -a ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp -a ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
 
-# TODO: Make device nodes
+# Make device nodes
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/console c 5 1
 
-# TODO: Clean and build the writer utility
+# Clean and build the writer utility
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${current_directory}/finder-app/writer ${OUTDIR}/rootfs/home/
+cp ${current_directory}/finder-app/finder.sh ${OUTDIR}/rootfs/home/
+cp ${current_directory}/finder-app/conf/ ${OUTDIR}/rootfs/home/
+cp ${current_directory}/finder-app/finder-test.sh ${OUTDIR}/rootfs/home/
+cp ${current_directory}/finder-app/autorun-qemu.sh ${OUTDIR}/rootfs/home/
 
-# TODO: Chown the root directory
+# Chown the root directory
+sudo chown -R root:root ${OUTDIR}/rootfs
 
-# TODO: Create initramfs.cpio.gz
+# Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${outdir}/initramfs.cpio
+gzip -f initramfs.cpio
+#COMMENT
