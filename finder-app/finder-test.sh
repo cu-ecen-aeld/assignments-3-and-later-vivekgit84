ac893ce403ec37b0a7e4dcebc112156a76ad3396
vtewari <vtewari@Ubuntu.myguest.virtualbox.org>
2024-12-28 21:45:18 -0700

    update

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -7,8 +7,8 @@ set -u
 
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp
-username=$(cat ../../etc/finder-app/conf/username.txt)
+WRITEDIR=/tmp/aeld-data
+username=$(cat cat ../../etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -22,49 +22,47 @@ then
 else
 	NUMFILES=$1
 	WRITESTR=$2
-	WRITEDIR=/tmp/$3
+	WRITEDIR=/tmp/aeld-data/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
 
 echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
-#rm -rf "${WRITEDIR}"
+rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
 assignment=`cat ../../etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
-	if [ ! -d "$WRITEDIR" ]
-	then
-		mkdir -p "$WRITEDIR"
+	mkdir -p "$WRITEDIR"
 
-		#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-		#The quotes signify that the entire string in WRITEDIR is a single string.
-		#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-		if [ -d "$WRITEDIR" ]
-		then
-			echo "$WRITEDIR created"
-		else
-			exit 1
-		fi
+	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
+	#The quotes signify that the entire string in WRITEDIR is a single string.
+	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
+	if [ -d "$WRITEDIR" ]
+	then
+		echo "$WRITEDIR created"
+	else
+		exit 1
 	fi
 fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/assignment4-result.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
-#rm -rf /tmp/aeld-data
+rm -rf /tmp/aeld-data
 
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
+	echo ${OUTPUTSTRING} > "/tmp/assignment4-result.txt"
 	echo "success"
 	exit 0
 else
